e95f71e940d76105839536783a08054ac244ecc6
Nyenzo <nyenzoisabwa@gmail.com>
2024-01-09 08:42:25 -0800

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-#configure ssh config
+#!/usr/bin/env bash
+# using puppet to make config changes
+file { 'etc/ssh/ssh_config'':
+	ensure => present,
 
-file_line{'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no'
-}
+content =>"
 
-file_line{'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school'
+	#SSH client configuration 
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
 }
